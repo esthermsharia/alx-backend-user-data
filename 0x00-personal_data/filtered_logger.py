165702e80dcae8365f75e5b426e798b4b5cbb9ab
kimengu-david <davidmutune200@gmail.com>
2021-08-19 21:00:27 +0000

    Remove pep8 style error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 from typing import List
 import re
 
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Returns a log message obfuscated"""
