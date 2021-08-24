76d5b49d925f42ec85c9b530c8d68c09cbe31593
kimengu-david <davidmutune200@gmail.com>
2021-08-24 19:49:03 +0000

    Add task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basic Authentication"""
 
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,19 @@ class BasicAuth(Auth):
             return None
         authstr = authorization_header.split(' ')
         return authstr[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """Decodes a base 64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            encode_utf8 = base64_authorization_header.encode('utf-8')
+            decoded = b64decode(encode_utf8)
+            actual_value = decoded.decode('utf-8')
+            return actual_value
+        except Exception:
+            return None
