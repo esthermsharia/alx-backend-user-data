27a69423bca2b8c1421553e7affc769a5e1ebc37
kimengu-david <davidmutune200@gmail.com>
2021-09-02 20:31:11 +0000

    Add task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,8 @@
     The returned bytes is a salted hash of the input password
 """
 import bcrypt
+from db import DB
+from user import User
 salt = bcrypt.gensalt()
 
 
@@ -10,3 +12,22 @@ def _hash_password(password: str) -> bytes:
     """Hashes a password"""
     hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(email: str, password: str) -> User:
+        """REgisters a user to the database"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            pwd = _hash_password(password)
+            user = self._db.add_user(email, pwd)
+            return user
+        else:
+            raise ValueError('User {email} already exists')
