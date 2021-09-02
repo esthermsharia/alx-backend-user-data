0078ae7da41117ccd865cb456f6a0e62fcac4e95
kimengu-david <davidmutune200@gmail.com>
2021-09-02 20:37:30 +0000

    Import 'NoResultFound'

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,7 +21,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(email: str, password: str) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """REgisters a user to the database"""
         try:
             user = self._db.find_user_by(email=email)
